a7f793a865b06048eb59da4e5330085eb99f50a5
021120 <yongtatt981213@hotmail.com>
2021-06-12 17:35:47 +0800

    Done

--- Classification.py
+++ Classification.py
@@ -4,7 +4,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
-import graphviz
 from IPython.display import Image
 import pydotplus
 from sklearn.tree import export_graphviz
@@ -14,9 +13,9 @@ dataset = pd.read_csv("Students' Academic Performance.csv")
 
 print(dataset)
 
-# CONVERT NON-NUMERICAL DATA INTO NUMERICAL DATA
+# CONVERT CATEGORICAL DATA INTO NUMERICAL DATA
 category_data = ["gender", 'NationalITy', 'PlaceofBirth', 'StageID', 'GradeID', 'SectionID', 'Topic',
-                 'Semester', 'Relation', 'ParentAnsweringSurvey', 'ParentschoolSatisfaction', 'StudentAbsenceDays', 'Class']
+                 'Semester', 'Relation', 'ParentAnsweringSurvey', 'ParentschoolSatisfaction', 'StudentAbsenceDays']
 
 encoder = LabelEncoder()
 for i in category_data:
@@ -29,7 +28,7 @@ print(dataset.dtypes)
 
 # determine independent variable(x) and dependent variable(y)
 independent_variable = ['gender', 'NationalITy', 'PlaceofBirth', 'StageID', 'GradeID', 'SectionID', 'Topic',
-                        'Semester', 'Relation', 'ParentAnsweringSurvey', 'ParentschoolSatisfaction', 'StudentAbsenceDays', ]
+                        'Semester', 'Relation', 'ParentAnsweringSurvey', 'ParentschoolSatisfaction', 'StudentAbsenceDays']
 
 dependent_variable = ['Class']
 
@@ -41,3 +40,48 @@ print("This is training dataset model (80%):")
 print(x_train)
 print("This is testing dataset model (20%):")
 print(x_test)
+
+# DO LOGISTIC REGRESSION MODEL
+logistic_regression = LogisticRegression(solver="lbfgs",
+                                         multi_class="auto", max_iter=5000)
+logistic_regression.fit(x_train, y_train)
+
+# SHOW TRAINING DATASET
+y_trainPrediction = logistic_regression.predict(x_train)
+print(x_train)
+print("Prediction list for training dataset: ")
+print(y_trainPrediction)
+
+# SHOW ACCURACY OF PREDICTION FOR TRAINING DATASET
+print("Accuracy of the prediction for the training dataset: ",
+      metrics.accuracy_score(y_train, y_trainPrediction))
+
+# SHOW TESTING DATASET
+y_testPrediction = logistic_regression.predict(x_test)
+print(x_test)
+print("Prediction list for testing dataset: ")
+print(y_testPrediction)
+
+# SHOW ACCURACY OF PREDICTION FOR TESTING DATASET
+print("Accuracy of the prediction for the testing dataset: ",
+      metrics.accuracy_score(y_test, y_testPrediction))
+
+# SHOW TRAINING DATASET DECISION TREE
+tree_classifier = DecisionTreeClassifier(criterion="entropy")
+tree_classifier.fit(x_train, y_train)
+dot_data = export_graphviz(tree_classifier, out_file=None,
+                           filled=True, rounded=False,
+                           special_characters=True, feature_names=independent_variable)
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_jpg('training.jpg')
+Image(graph.create_jpg())
+
+# SHOW TESTING DATASET DECISION TREE
+tree_classifier = DecisionTreeClassifier(criterion="entropy")
+tree_classifier.fit(x_test, y_test)
+dot_data1 = export_graphviz(tree_classifier, out_file=None,
+                            filled=True, rounded=False,
+                            special_characters=True, feature_names=independent_variable)
+graph = pydotplus.graph_from_dot_data(dot_data1)
+graph.write_jpg('testing.jpg')
+Image(graph.create_jpg())
